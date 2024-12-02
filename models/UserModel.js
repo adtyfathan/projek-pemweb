052fdf6cab4962258bc9f4beb0b677393b04193f
adtyfathan <adtyfathan04@gmail.com>
2024-12-02 12:43:14 +0700

    tambah

--- models/UserModel.js
+++ models/UserModel.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true },
     username: { type: String, required: true },
     password: { type: String, required: true },
-    image: { type: String, default: "/images/profile-default.png" },
+    image: { type: String, default: "/images/amba.jpg" },
     role: { type: String, required: true },
     address: { type: String, default: null },
     country: { type: String, default: null },
